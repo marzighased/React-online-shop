2af3513cc044f0ffd5d0beb1fc85e8ff17e98eea
Marzi Ghasedian <marzighased@gmail.com>
2025-07-02 17:31:51 +0200

    navbar changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/navbar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World</h1>);
+root.render(<Navbar/>);
