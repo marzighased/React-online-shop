5766220d2f01e387f8627c9078a1fa54c1470b45
Marzi Ghasedian <marzighased@gmail.com>
2025-07-03 20:25:32 +0200

    navbar bootstrap style added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Navbar/>);
