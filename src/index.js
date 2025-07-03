19754bf0d21bb71cc1a7dbd9c3910f9e431d53fe
Marzi Ghasedian <marzighased@gmail.com>
2025-07-03 21:00:55 +0200

    product card with bootstrap style added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Product from './components/product';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Navbar/>);
+root.render(<Product/>);
