07b9a3fb019a1ddc3b7912b4fd19f0ae7ec89464
Marzi Ghasedian <marzighased@gmail.com>
2025-07-04 15:11:06 +0200

    root (App.jsx) added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Navbar from './components/navbar'
+import Product from './components/product';
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return <React.Fragment>
+                   <Navbar/>
+                   <div>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                   </div>
+                   
+               </React.Fragment>;
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/navbar'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Product from './components/product';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Product/>);
+root.render(<App></App>);
