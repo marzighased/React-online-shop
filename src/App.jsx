fdfec6c269190ef4fc7a9f1f9f18a5f5faf02ff9
Marzi Ghasedian <marzighased@gmail.com>
2025-07-05 22:16:07 +0200

    Product details added

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ class App extends Component {
         return <React.Fragment>
                    <Navbar/>
                    <div className='product-container'>
-                        <Product/>
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <Product image="tomatos.jpg" title="Tomaten" description="Füge Tomaten zu deinem Warenkorb hin."/>
+                        <Product image="cucumbers.jpg" title="Gurken" description="Füge Gurken zu deinem Warenkorb hin."/>
+                        <Product image="apples.jpg" title="Äpfel"  description="Füge Äpfel zu deinem Warenkorb hin."/>
+                        <Product image="birne.jpg" title="Birnen"  description="Füge Birne zu deinem Warenkorb hin."/>
                    </div>
                    
                </React.Fragment>;
