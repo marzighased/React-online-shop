bb3de78f60efa5d5d10a9fb51539aa7ac785bf4a
Marzi Ghasedian <marzighased@gmail.com>
2025-07-06 21:50:50 +0200

    shopping cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import Navbar from './components/navbar'
 import Product from './components/product';
+import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
     state = {  } 
     render() { 
         return <React.Fragment>
                    <Navbar/>
-                   <div className='product-container'>
-                        <Product image="tomatos.jpg" title="Tomaten" description="Füge Tomaten zu deinem Warenkorb hin."/>
-                        <Product image="cucumbers.jpg" title="Gurken" description="Füge Gurken zu deinem Warenkorb hin."/>
-                        <Product image="apples.jpg" title="Äpfel"  description="Füge Äpfel zu deinem Warenkorb hin."/>
-                        <Product image="birne.jpg" title="Birnen"  description="Füge Birne zu deinem Warenkorb hin."/>
+                   <div className='main-container'>
+                        <div className='product-container'>
+                            <Product image="tomatos.jpg" title="Tomaten" description="Füge Tomaten zu deinem Warenkorb hin."/>
+                            <Product image="cucumbers.jpg" title="Gurken" description="Füge Gurken zu deinem Warenkorb hin."/>
+                            <Product image="apples.jpg" title="Äpfel"  description="Füge Äpfel zu deinem Warenkorb hin."/>
+                            <Product image="birne.jpg" title="Birnen"  description="Füge Birne zu deinem Warenkorb hin."/>
+                        </div>
+                        <ShoppingCart/>
                    </div>
-                   
                </React.Fragment>;
     }
 }
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,7 +8,7 @@ class Product extends Component {
         <div className="card-body">
             <h5 className="card-title">{this.props.title}</h5>
             <p className="card-text">{this.props.description}</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <a href="/" className="btn btn-primary">Hinzufügen</a>
         </div>
         </div>;
     }
