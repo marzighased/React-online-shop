5f67a07481976788f335c034848b0cf85cea3628
Marzi Ghasedian <marzighased@gmail.com>
2025-07-07 14:18:16 +0200

    onAdd command added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
                    <Navbar/>
                    <div className='main-container'>
                         <div className='product-container'>
-                            <Product image="tomatos.jpg" title="Tomaten" description="Füge Tomaten zu deinem Warenkorb hin."/>
+                            <Product onAdd={addItem(1, 'Tomaten', 2.99)} image="tomatos.jpg" title="Tomaten" description="Füge Tomaten zu deinem Warenkorb hin."/>
                             <Product image="cucumbers.jpg" title="Gurken" description="Füge Gurken zu deinem Warenkorb hin."/>
                             <Product image="apples.jpg" title="Äpfel"  description="Füge Äpfel zu deinem Warenkorb hin."/>
                             <Product image="birne.jpg" title="Birnen"  description="Füge Birne zu deinem Warenkorb hin."/>
