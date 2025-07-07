741d71f5b5ce79f06de65c951578bcabecd0be60
Marzi Ghasedian <marzighased@gmail.com>
2025-07-07 14:01:10 +0200

    shopping cart render function added

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 class ShoppingCart extends Component {
-    state = {  } 
+    state = { 
+        items: [
+            {
+                amount: 1,
+                name: 'Tomaten',
+                price: 2.9
+            },
+            {
+                amount: 1,
+                name: 'Äpfel',
+                price: 4.9
+            }
+        ]
+    }
+
     render() { 
         return <div className='shopping-cart'>
             <h2>Warenkorb</h2>
+            { this.state.items.map(item => <div key={item.name}>{ item.amount }x { item.name } { item.price }€</div>)}
         </div>;
     }
 }
